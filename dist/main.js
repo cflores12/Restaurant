fa8aaba50375f02245f2e41231fe5a99c8cec1e8
flores <cflores12@users.noreply.github.com>
2023-06-08 20:16:27 -0400

    start work on nav menu

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.log("Hello This is Breakfast at Bryant!"),document.querySelector("body"),document.querySelector("div#content");
---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log("Hello This is Breakfast at Bryant!");
+const body = document.querySelector("body");
+const content = document.querySelector("div#content");
+
+function header() {
+    const header = document.createElement('header');
+    const nav = document.createElement('nav');
+
+}
