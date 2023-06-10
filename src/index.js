c2b06781916ac02c0c6dbccc05cc3ed6838abc2b
flores <cflores12@users.noreply.github.com>
2023-06-10 09:22:04 -0400

    start navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-console.log("Hello This is Breakfast at Bryant!");
 const body = document.querySelector("body");
 const content = document.querySelector("div#content");
 
 function header() {
-    const header = document.createElement('header');
-    const nav = document.createElement('nav');
+  const header = document.createElement("header");
+  const nav = document.createElement("nav");
+  const ul = document.createElement("ul");
 
+  const menu = document.createElement("li");
+  menu.textContent = "Menu";
+
+  const contact = document.createElement("li");
+  contact.textContent = "Contact";
+
+  const home = document.createElement("li");
+  home.textContent = "Home";
+
+  ul.appendChild(menu);
+  ul.appendChild(home);
+  ul.appendChild(contact);
+  nav.appendChild(ul);
+  header.appendChild(nav);
+  body.appendChild(header);
 }
+
+function menu() {}
+
+function contact() {}
+
+function home() {}
